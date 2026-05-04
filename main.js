d7d6fa291048e849d437e13a26f6dfaa2db4ca10
5run4 <araujo.amaral@escola.pr.gov.br>
2026-05-04 08:25:08 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,18 +22,6 @@ const tempoObjetivo4 = new Date("2027-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-// Atualizar contadores na primeira vez
-for (let i = 0; i < contadores.length; i++) {
-    contadores[i].textContent = calculaTempo(tempos[i]);
-}
-
-// Atualizar a cada 1 segundo
-setInterval(function() {
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
-    }
-}, 1000);
-
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -46,5 +34,22 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
 
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    if(tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {
+        return "Prazo Finalizado";
+    }
 }
+
+function atualizaCronometro(){ 
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
