4b09f5df6f31586ddcd4946423da1817b9b3fe91
5run4 <araujo.amaral@escola.pr.gov.br>
2026-05-11 08:39:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for(let i=0; i < botoes.length; i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2027-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2026-11-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-08-01T00:00:00");
+const tempoObjetivo3 = new Date("2026-11-05T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-18T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
@@ -35,15 +35,20 @@ function calculaTempo(tempoObjetivo){
     horas %= 24;
 
     if(tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+        return [dias,horas,minutos,segundos];
     } else {
-        return "Prazo Finalizado";
+        return [0,0,0,0];
     }
 }
 
 function atualizaCronometro(){ 
+
     for (let i = 0; i < contadores.length; i++) {
-        contadores[i].textContent = calculaTempo(tempos[i]);
+        document.getElementById("Dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("Horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("Min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("Seg"+i).textContent = calculaTempo(tempos[i])[3];
+        console.log("Seg"+i);
     }
 }
 
@@ -52,4 +57,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro, 1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
